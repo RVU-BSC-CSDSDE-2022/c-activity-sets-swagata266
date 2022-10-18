784e4a37c3967bf194b76eeac7a1f26ec5474a66
Swagata Singha  Singha <114578729+swagata266@users.noreply.github.com>
2022-10-18 06:30:09 +0000

    commited

---
+++ set01/problem02.c
@@ -0,0 +1,11 @@
+Write a C program to add two numbers.clear
+#include <stdio.h>
+int main()
+{
+	int a,b,c;
+	printf(“Enter two number\n”);
+	scanf(“%d%d”,&a,&b);
+	c=a+b;
+	printf(“%d + %d is %d\n”,a,b,c);
+	return 0;
+}
